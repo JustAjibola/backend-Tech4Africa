fb80af74e509b341c507e4f702ba9fdc485c5435
JustAjibola <akinsola51@gmail.com>
2026-04-13 20:00:06 +0100

    run

--- error.js
+++ error.js
@@ -1,16 +1,17 @@
 //syntax error
 //let add = (5 + 3 
-    //console.log (add)
+//console.log (add)
 
 
-    //reference error
-    let myame = "Tolu";
-    console.log(myName);
+//reference error
+let myame = "Tolu";
+console.log(myName);
 
 //type error
 let num = 5;
 console.log(num.toUpperCase());
 
+//try-catch example
 try {
 
 } catch (error) {
@@ -19,25 +20,26 @@ try {
 
 try {
     //code that might fail goes here
-    let result = 10 / 0; 
+    let result = 10 / 0;
     console.log(result);
     undefinedFunction(); // This will throw a ReferenceError
     console.log("This will not be executed due to the error above.");
 } catch (error) {
-    console.log("An error occurred: " + undefinedFunction is not defined);
+    console.log("An error occurred: " + error.message);
 }
 
-console.log("This will still be executed even if there was an error in the try block.");    
+console.log("This will still be executed even if there was an error in the try block.");
 
 
-try{
-let age = 25;
-console.log(age.lenth());
-}catch(error)
-   console.log("An error occurred: " + error.message);
+try {
+    let age = 25;
+    console.log(age.lenth());
+} catch (error) {
+    console.log("An error occurred: " + error.message);
+}
 
 
-function loadUserData(usderId) {
+function loadUserData(userId) {
     console.log("Starting to load user data...");
     try {
         if (!userId.startsWith("BAD")) {
@@ -48,4 +50,5 @@ function loadUserData(usderId) {
         console.log("Failed: " + error.message);
     } finally {
         console.log("loading complete. Closing connection.");
-    }");
+    }
+}
